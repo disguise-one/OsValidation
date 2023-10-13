8de45db77775107a6c93829bbf871a7624c110bf
dv1sual <53606943+dv1sual@users.noreply.github.com>
2023-10-13 11:07:45 +0100

    Readme update and gitignore

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,7 @@ import logging
 import logging.handlers
 
 # Set up logging
-log_file = "C:\\Users\\d3\\PycharmProjects\\os_check_test\\logs\\os_test_run.log"
+log_file = "D:\\code\\OsValidation\\logs\\os_test_run.log"
 logging.basicConfig(level=logging.DEBUG, format='| %(levelname)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=100000, backupCount=5)
 handler.setLevel(logging.DEBUG)
