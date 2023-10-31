3cfec702714df33a56288fbbc018378766e0ad37
dv1sual <53606943+dv1sual@users.noreply.github.com>
2023-10-31 12:34:37 +0000

    Refactoring the d3_interaction.py to not use the AutoGUI anymore, instead using windows classes. Now we are free of running a specific resolution to match hardcoded coordinates.

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # OsValidation
-
 OS Validation Automated Test
-This Python script is designed for performing various validation checks on a Windows operating system. It checks different aspects such as Windows settings, file handling, device configurations, and interactions with a D3 system.
-
-For more detailed description check the wiki.
--- utils/d3_interaction.py
+++ utils/d3_interaction.py
@@ -1,87 +1,84 @@
-import time
-import pyautogui
+import os
 import subprocess
+from pywinauto.application import Application
+import time
 
 
-def check_d3_project():
-    print("| C62863 | Checking for the d3 Project folder, please wait...")
-    time.sleep(2)
-    d3_manager_path = "C:\\Program Files\\d3 Production Suite\\build\\msvc\\d3manager.exe"
-    subprocess.Popen(d3_manager_path)
+def check_d3_projects():
+    folder_path = "D:\\d3 Projects"
 
-    while True:
-        input_str = input("| C62863 | Is d3 Project available on the D drive? (Y/N): ")
-        if input_str.lower() == 'y':
-            print("| C62863 | d3 Project check passed")
-            break
-        elif input_str.lower() == 'n':
-            print("| C62863 | d3 Project check failed")
-            break
-        else:
-            print("| C62863 | Invalid input. Please enter 'y' or 'n'.")
-
-
-def check_d3_manager_help():
-    print("| C62864 | Checking for the d3 manager help, please wait...")
-    time.sleep(2)
-    d3_manager_path = "C:\\Program Files\\d3 Production Suite\\build\\msvc\\d3manager.exe"
-    subprocess.Popen(d3_manager_path)
-
-    time.sleep(5)
-    # click the 'x' button to close any existing instances of the d3 manager help
-    pyautogui.click(x=876, y=130, duration=0.5)
-    time.sleep(5)
-    # open the d3 manager help
-    pyautogui.click(x=963, y=159, duration=0.5)
-    time.sleep(2)  # wait for the help to open
-
-    # ask the user if the d3 manager help opened
-    response = input("| C62864 | Did the d3 manager help open? (Y/N) ")
-    if response.upper() == 'Y':
-        print("| C62864 | d3 Manager help check passed.")
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        print("| C62865 | d3 Projects check passed: 'd3 Projects' folder exists on D: drive.")
     else:
-        print("| C62864 | d3 Manager help check failed.")
+        print("| C62865 | d3 Projects check failed: 'd3 Projects' folder not found on D: drive.")
 
 
-def check_d3_licences():
-    print("| C62865 | Checking for the d3 licences, please wait...")
-    time.sleep(2)
+def check_d3_manager_tests():
+    # Initialize the d3 manager path
     d3_manager_path = "C:\\Program Files\\d3 Production Suite\\build\\msvc\\d3manager.exe"
+
+    # Launch d3 manager
+    print("| INFO | Launching d3 manager, please wait...")
     subprocess.Popen(d3_manager_path)
+    time.sleep(10)  # Delay to ensure the application window is fully initialized
 
-    time.sleep(5)
-    # click the 'x' button to close any existing instances of the d3 manager help
-    pyautogui.click(x=876, y=130, duration=0.5)
-    time.sleep(5)
-    # open the d3 manager help
-    pyautogui.click(x=932, y=381, duration=0.5)
-    time.sleep(2)  # wait for the help to open
+    # Connect to the d3manager application using pywinauto
+    app = Application(backend="uia").connect(path=d3_manager_path)
+    main_win = app.window(class_name="D3Manager")
 
-    # ask the user if the d3 manager help opened
+    # Expand the Help menu
+    help_menu = main_win.child_window(title="Help", control_type="MenuItem")
+    help_menu.click_input()
+
+    # Wait a bit for the submenu to appear
+    time.sleep(5)
+
+    # Check d3 manager help
+    print("| C62864 | Checking for the d3 manager help...")
+    d3manager_help = main_win.child_window(auto_id="D3ManagerClass.actionHelp", control_type="MenuItem")
+    d3manager_help.click_input()
+    response = input("| C62864 | Did the d3 manager help open? (Y/N) ")
+    print("| C62864 | d3 Manager help check " + ("passed." if response.upper() == 'Y' else "failed."))
+
+    # Refocus on the main window and expand the Help menu again
+    main_win.set_focus()
+    help_menu.click_input()
+    time.sleep(5)
+
+    # Check d3 licenses
+    print("| C62865 | Checking for the d3 licences...")
+    for _ in range(3):  # Try 3 times
+        try:
+            d3_licenses = main_win.child_window(auto_id="D3ManagerClass.actionLicense", control_type="MenuItem")
+            d3_licenses.wait('visible', timeout=10)
+            d3_licenses.click_input()
+            break
+        except Exception as e:
+            print(f"| ERROR | Retry accessing d3 Licenses due to: {str(e)}")
+    else:
+        print("| ERROR | Failed to access d3 Licenses after multiple attempts.")
+        return
+
+    # Ask the user if the d3 licences window opened and the licence was found
     response = input("| C62865 | Did the d3 licences window open, and the licence was found? (Y/N) ")
     if response.upper() == 'Y':
         print("| C62865 | d3 licences check passed.")
     else:
         print("| C62865 | d3 licences check failed.")
 
-
-def check_OS_image_version():
-    print("| C62865 | Checking for the OS image version, please wait...")
-    time.sleep(2)
-    d3_manager_path = "C:\\Program Files\\d3 Production Suite\\build\\msvc\\d3manager.exe"
-    subprocess.Popen(d3_manager_path)
-
+    # Refocus on the main window and expand the Help menu again
+    main_win.set_focus()
+    help_menu.click_input()
     time.sleep(5)
-    # click the 'x' button to close any existing instances of the d3 manager help
-    pyautogui.click(x=876, y=130, duration=0.5)
-    time.sleep(5)
-    # open the d3 manager help
-    pyautogui.click(x=918, y=475, duration=0.5)
-    time.sleep(2)  # wait for the help to open
 
-    # ask the user if the d3 manager help opened
+    # Check OS image version
+    print("| C62865 | Checking for the OS image version...")
+    about_d3manager = main_win.child_window(auto_id="D3ManagerClass.actionAbout", control_type="MenuItem")
+    about_d3manager.click_input()
     response = input("| C62864 | Did the About d3 manager window open, and the OS image Version was found? (Y/N) ")
-    if response.upper() == 'Y':
-        print("| C62864 | Machine OS image version check passed.")
-    else:
-        print("| C62864 | Machine OS image version check failed.")
+    print("| C62864 | Machine OS image version check " + ("passed." if response.upper() == 'Y' else "failed."))
+
+    # Close the d3 manager
+    print("| INFO | Closing d3 manager...")
+    close_button = main_win.child_window(title="Close", control_type="Button")
+    close_button.click()
