112f2d1a3f2a39f39ef596ea15fe3b94773d9c3c
dv1sual <53606943+dv1sual@users.noreply.github.com>
2023-11-10 10:53:23 +0000

    - Merged d3_interaction refactor - Un-Commented the main.py

--- main.py
+++ main.py
@@ -3,78 +3,53 @@ from utils.logger import logging
 import time
 import sys
 
-sleep_time = 1
+try:
+    logging.info("Running Windows Settings Validation, please wait...")
+    windows_settings.check_taskbar_icons()
+    windows_settings.check_start_menu_tiles()
+    windows_settings.check_windows_licensing()
+    windows_settings.check_windows_background_color()
+    windows_settings.check_machine_name()
+    windows_settings.check_notifications_disabled()
+    windows_settings.check_sticky_keys_disabled()
+    windows_settings.check_windows_firewall_disabled()
+except KeyboardInterrupt:
+    logging.info("Keyboard Interrupt received, exiting the program.")
+    sys.exit(0)
+except Exception as e:
+    logging.exception(f"An unexpected error occurred: {e}")
 
-# try:
-#     logging.info("Running Windows Settings Validation, please wait...")
-#     time.sleep(2)
-#     windows_settings.check_taskbar_icons()
-#     time.sleep(sleep_time)
-#     windows_settings.check_start_menu_tiles()
-#     time.sleep(sleep_time)
-#     windows_settings.check_windows_licensing()
-#     time.sleep(sleep_time)
-#     windows_settings.check_windows_background_color()
-#     time.sleep(sleep_time)
-#     windows_settings.check_machine_name()
-#     time.sleep(sleep_time)
-#     windows_settings.check_notifications_disabled()
-#     time.sleep(sleep_time)
-#     windows_settings.check_sticky_keys_disabled()
-#     time.sleep(sleep_time)
-#     windows_settings.check_windows_firewall_disabled()
-#     time.sleep(2)
-# except KeyboardInterrupt:
-#     logging.info("Keyboard Interrupt received, exiting the program.")
-#     sys.exit(0)
-# except Exception as e:
-#     logging.exception(f"An unexpected error occurred: {e}")
-#
-# try:
-#     logging.info("Running File Handling Validation, please wait...")
-#     time.sleep(2)
-#     file_handling.check_system_failure_checkboxes()
-#     time.sleep(sleep_time)
-#     file_handling.check_dump_file_path()
-#     time.sleep(sleep_time)
-#     file_handling.check_file()
-# except KeyboardInterrupt:
-#     logging.info("Keyboard Interrupt received, exiting the program.")
-#     sys.exit(0)
-# except Exception as e:
-#     logging.exception(f"An unexpected error occurred: {e}")
-#
-# try:
-#     logging.info("Running Devices Validation, please wait...")
-#     time.sleep(2)
-#     devices.check_general_devices()
-#     time.sleep(sleep_time)
-#     devices.check_gpu_devices()
-#     time.sleep(sleep_time)
-#     devices.check_network_devices()
-#     time.sleep(sleep_time)
-#     devices.check_deltacast_devices()
-#     time.sleep(sleep_time)
-#     devices.check_matrox_devices()
-#     time.sleep(sleep_time)
-#     devices.check_audio_devices()
-#     time.sleep(sleep_time)
-#     devices.check_audio_card_management()
-#     time.sleep(sleep_time)
-#     devices.check_media_drives()
-#     time.sleep(sleep_time)
-#     devices.check_raid_tool()
-# except KeyboardInterrupt:
-#     logging.info("Keyboard Interrupt received, exiting the program.")
-#     sys.exit(0)
-# except Exception as e:
-#     logging.exception(f"An unexpected error occurred: {e}")
+try:
+    logging.info("Running File Handling Validation, please wait...")
+    file_handling.check_system_failure_checkboxes()
+    file_handling.check_dump_file_path()
+    file_handling.check_file()
+except KeyboardInterrupt:
+    logging.info("Keyboard Interrupt received, exiting the program.")
+    sys.exit(0)
+except Exception as e:
+    logging.exception(f"An unexpected error occurred: {e}")
+
+try:
+    logging.info("Running Devices Validation, please wait...")
+    devices.check_general_devices()
+    devices.check_gpu_devices()
+    devices.check_network_devices()
+    devices.check_deltacast_devices()
+    devices.check_matrox_devices()
+    devices.check_audio_devices()
+    devices.check_audio_card_management()
+    devices.check_media_drives()
+    devices.check_raid_tool()
+except KeyboardInterrupt:
+    logging.info("Keyboard Interrupt received, exiting the program.")
+    sys.exit(0)
+except Exception as e:
+    logging.exception(f"An unexpected error occurred: {e}")
 
 try:
     logging.info("Running d3 interaction validation, please wait...")
-    time.sleep(2)
     d3_interaction.check_d3_projects()
-    time.sleep(sleep_time)
     d3_interaction.check_d3_manager_tests()
 except KeyboardInterrupt:
     logging.info("Keyboard Interrupt received, exiting the program.")
